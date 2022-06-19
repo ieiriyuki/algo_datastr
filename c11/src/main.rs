49cf3ee637217008b578cd42553d912566f47924
yuki.ieiri <ieiriyuki@sur.bbiq.jp>
2022-06-19 17:53:49 +0900

    Update question 11-1 is done

--- c11/src/main.rs
+++ c11/src/main.rs
@@ -11,21 +11,59 @@ fn main() {
         (5, 6),
         (6, 7),
     ];
-    q11_1(&a);
+    println!("n bridges: {}", q11_1(&a));
+    let a = vec![
+        (1, 2),
+        (1, 3),
+        (2, 3),
+    ];
+    println!("n bridges: {}", q11_1(&a));
+    let a = vec![
+        (1, 2),
+        (2, 3),
+        (3, 4),
+        (4, 5),
+        (5, 6),
+    ];
+    println!("n bridges: {}", q11_1(&a));
+
+    println!("\nquestion 11-2");
 }
 
 fn q11_1(v_: &Vec<(usize, usize)>) -> i32 {
     let m = v_.len();
     let n = get_num_of_elem(v_);
-    println!("{}, {}", m, n);
-    let mut uf = UnionFind::new(n);
-    println!("{:?}", uf);
-
-    for (a, b) in v_.into_iter() {
-        let _ = uf.unite(*a, *b);
-        println!("{:?}", uf);
+    let mut n_bridge= 0;
+    let mut bridges: Vec<(usize, usize)> = Vec::new();
+    // v_ 中の辺 i を除いたときに, 全ての点の根が同じかどうか
+    for i in 0..m {
+        let mut v = v_.clone();
+        v.remove(i);
+        // i 以外の辺で UnionFind を作る
+        let mut uf = UnionFind::new(n);
+        for (a, b) in v.into_iter() {
+            uf.unite(a, b);
+        }
+        // 全ての点の根が同じだったら, 橋ではない
+        let mut are_all_same = true;
+        for j in 1..n {
+            for k in j..=n {
+                are_all_same = uf.issame(j, k);
+                if ! are_all_same {
+                    break
+                }
+            }
+            if ! are_all_same {
+                break
+            }
+        }
+        if ! are_all_same {
+            bridges.push(v_[i]);
+            n_bridge += 1;
+        }
     }
-    return 1
+    println!("{:?}", bridges);
+    return n_bridge
 }
 
 fn get_num_of_elem(v: &Vec<(usize, usize)>) -> usize {
@@ -44,6 +82,7 @@ struct UnionFind {
     siz: HashMap<usize, usize>,
 }
 
+#[allow(dead_code)]
 impl UnionFind {
     fn new(n: usize) -> Self {
         UnionFind{
