db1e41714bbcddfb6cbbed99730c23c5abb26d2f
ieiriyuki <test@example.com>
2022-06-28 16:46:23 +0900

    Add code 13-2 written

--- c13/src/main.rs
+++ c13/src/main.rs
@@ -1,3 +1,40 @@
 fn main() {
-    println!("Hello, world!");
+    let g: Vec<Vec<usize>> = vec![
+        vec![5],
+        vec![3, 6],
+        vec![5, 7],
+        vec![0, 7],
+        vec![1, 2, 6],
+        vec![],
+        vec![7],
+        vec![0],
+    ];
+    c13_2(&g);
+}
+
+fn dfs(g: &Vec<Vec<usize>>, v: usize, seen: &mut Vec<bool>) {
+    seen[v] = true;
+    println!("{}", v);
+    for next_v in g[v].iter() {
+        if seen[*next_v] {
+            continue
+        }
+        dfs(g, *next_v, seen);
+    }
+}
+
+fn c13_2(g: &Vec<Vec<usize>>) {
+    println!("code 13-2");
+    let mut seen = vec![false; 8];
+    for i in 0..7 {
+        dfs(g, i as usize, &mut seen);
+    }
+}
+
+fn bfs(g: &Vec<Vec<usize>>, v: usize, seen: &mut Vec<bool>) {
+
+}
+
+fn c13_3() {
+    println!("code 13-3");
 }
